672f9832e1ca8c69c3e856eb49d120a894e67bec
Glazelf <gideon.steenbergen@gmail.com>
2019-02-25 14:47:23 +0100

    init

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# StringGenerator
+# TDTCountdown
+Some older, awful coding to make a quick countdown for [ThermoDynamic Throwdown](https://smash.gg/tournament/thermodynamic-throwdown-a-dedicated-pokken-major-by-team-calyptus).
+
+
+Should look somewhat like this: 
+![ExampleScreenshot](https://i.imgur.com/asv664U.jpg)
---
+++ assets/js/app.js
@@ -0,0 +1,32 @@
+//generate 4 random unique numbers
+var number1 = null;
+var number2 = null;
+var number3 = null;
+var number4 = null;
+do {
+  number1 = Math.floor(Math.random() * 10);
+  number2 = Math.floor(Math.random() * 10);
+  number3 = Math.floor(Math.random() * 10);
+  number4 = Math.floor(Math.random() * 10);
+} while (number1 == number2 || number1 == number3 || number1 == number4 || number2 == number3 || number2 == number4 || number3 == number4);
+var totalNumber = `${number1}${number2}${number3}${number4}`;
+document.getElementById("numbers").textContent = `${totalNumber}`;
+
+//generate 4 random unique characters
+var possible = "abcd";
+var character1 = null;
+var character2 = null;
+var character3 = null;
+var character4 = null;
+do{
+  var character1 = null;
+  var character2 = null;
+  var character3 = null;
+  var character4 = null;
+  character1 += possible.charAt(Math.floor(Math.random() * 4))
+  character2 += possible.charAt(Math.floor(Math.random() * 4))
+  character3 += possible.charAt(Math.floor(Math.random() * 4))
+  character4 += possible.charAt(Math.floor(Math.random() * 4))
+} while (character1 == character2 || character1 == character3 || character1 == character4 || character2 == character3 || character2 == character4 || character3 == character4);
+var totalCharacters = `${character1}${character2}${character3}${character4}`
+document.getElementById("letters").textContent = `${totalCharacters}`
