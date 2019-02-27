56b62e0481e574b2f74c9a613e3576d24b8ef7b2
Glazelf <gideon.steenbergen@gmail.com>
2019-02-27 17:45:06 +0100

    initial commit

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,33 +1,31 @@
-//generate 4 random unique numbers
-var number1 = null;
-var number2 = null;
-var number3 = null;
-var number4 = null;
-do {
-  number1 = Math.floor(Math.random() * 10);
-  number2 = Math.floor(Math.random() * 10);
-  number3 = Math.floor(Math.random() * 10);
-  number4 = Math.floor(Math.random() * 10);
-} while (number1 == number2 || number1 == number3 || number1 == number4 || number2 == number3 || number2 == number4 || number3 == number4);
-var totalNumber = `${number1}${number2}${number3}${number4}`;
-document.getElementById("numbers").textContent = `${totalNumber}`;
+document.getElementById('body');
+let possible = "abcd";
+let character1;
+let character2;
+let character3;
+let character4;
+let previousSets = [];
+let indexNumber;
 
-//generate 4 random unique characters
-var possible = "abcd";
-function string() {
-  do {
-    character1 = possible.charAt(Math.floor(Math.random() * 4));
-    character2 = possible.charAt(Math.floor(Math.random() * 4));
-    character3 = possible.charAt(Math.floor(Math.random() * 4));
-    character4 = possible.charAt(Math.floor(Math.random() * 4));
-  } while (character1 == character2 || character1 == character3 || character1 == character4 || character2 == character3 || character2 == character4 || character3 == character4){
-  document.getElementById("letters").innerHTML += `${character1}${character2}${character3}${character4}<br>`;
-  console.log(2);
+function randomLetters() {
+  text = document.createElement('div');
+
+  for (let index = 0; index < previousSets.length + 2; index++) {
+    if (previousSets[index - 1] == `${character1}${character2}${character3}${character4}` || character1 == character2 || character1 == character3 || character1 == character4 || character2 == character3 || character2 == character4 || character3 == character4) {
+      character1 = possible.charAt(Math.floor(Math.random() * 4));
+      character2 = possible.charAt(Math.floor(Math.random() * 4));
+      character3 = possible.charAt(Math.floor(Math.random() * 4));
+      character4 = possible.charAt(Math.floor(Math.random() * 4));
+      index = 0
+    };
+    indexNumber = index;
   };
+
+  previousSets.push(`${character1}${character2}${character3}${character4}`);
+  text.innerText = `${indexNumber} ${character1}${character2}${character3}${character4}`;
+  body.appendChild(text);
 };
 
-var i = 0
-do {
-  string();
-  i++;
-} while (i < 20); 
+for (let index = 0; index < 24; index++) {
+  randomLetters();
+};
